d837456684d49b162c0408b44c7ef9765e34a00f
simon <simon.holden@gmail.com>
2019-04-21 18:28:08 +0100

    create master class

--- Cluedo/CluedoGame.py
+++ Cluedo/CluedoGame.py
@@ -2,7 +2,16 @@ import CluedoUtils as cu
 
 def main():
     print("CLUEDO")
-    cu.startGame(3)
-
+    cg=cu.startGame(3)
+    cg.revealSecret()
+    gameOn=True
+    while gameOn:
+        print("My hand:", cg.hands[0])
+        print("My Score Card:")
+        cg.gamecards[0].printme()
+        print("make a move")
+        print("ask a question")
+        print("check for answer")
+        gameOn=False
 
 main()
--- Cluedo/CluedoUtils.py
+++ Cluedo/CluedoUtils.py
@@ -32,38 +32,46 @@ class scoreCard:
         print(self.weaponMarks)
         print(self.peopleMarks)
 
+class CluedoGame:
+    def __init__(self,numPlayers):
+        self.test=""
+        # make a copy of the play lists that we can chop
+        cpyRooms = rooms
+        cpyPeople = people
+        cpyWeapons = weapons
+
+        # take one card from each list, concatenate and then shuffle
+        self.killer = cpyPeople.pop(rd.randint(0, len(cpyPeople) - 1))
+        self.murder_room = cpyRooms.pop(rd.randint(0, len(cpyRooms) - 1))
+        self.murder_weapon = cpyWeapons.pop(rd.randint(0, len(cpyWeapons) - 1))
+
+        self.deck = cpyRooms + cpyPeople + cpyWeapons
+        rd.shuffle(self.deck)
+
+        #   divide up the cards between numPlayers
+        self.hands = [[] for i in range(numPlayers)]
+        player = 1
+        while (len(self.deck) > 0):
+            card = self.deck.pop(rd.randint(0, len(self.deck) - 1))
+            self.hands[player - 1].append(card)
+            player = player + 1
+            if (player > numPlayers):
+                player = 1
+
+        # Set up gamecards for each player
+        self.gamecards = []
+        for i in range(numPlayers):
+            self.gamecards.append(scoreCard(self.hands[i]))
+
+    def revealSecret(self):
+        print("Top Secret:", self.killer, self.murder_room, self.murder_weapon)
+
 def startGame(numPlayers):
     assert numPlayers>1,"Must be more than 1 player"
 
-    #make a copy of the play lists that we can chop
-    cpyRooms=rooms
-    cpyPeople=people
-    cpyWeapons=weapons
+    cg=CluedoGame(numPlayers)
+    return cg
 
-    # take one card from each list, concatenate and then shuffle
-    killer=cpyPeople.pop(rd.randint(0,len(cpyPeople)-1))
-    murder_room=cpyRooms.pop(rd.randint(0,len(cpyRooms)-1))
-    murder_weapon=cpyWeapons.pop(rd.randint(0,len(cpyWeapons)-1))
-    print("Top Secret:", killer, murder_room,murder_weapon)
-    deck = cpyRooms + cpyPeople + cpyWeapons
-    rd.shuffle(deck)
-
-#   divide up the cards between numPlayers
-    hands=[[] for i in range(numPlayers)]
-    player=1
-    while (len(deck)>0):
-        card=deck.pop(rd.randint(0,len(deck)-1))
-        hands[player-1].append(card)
-        player=player+1
-        if (player>numPlayers):
-            player=1
-
-   # Print my hand
-    print("My hand:",hands[0])
-
-# Assume that i will always be player 1
-    gamecards=[]
-    for i in range(numPlayers):
-        gamecards.append(scoreCard(hands[i]))
-    print("My Score Card:")
-    gamecards[0].printme()
+def guess():
+#given a room + weapon + person, returns false if no player has any else the player number and card
+    print ("guess")
